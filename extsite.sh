015d70cfeba08616d07d4b8f87335a2ca904e2d7
Arthur Sant'Anna <arthursantanna.c@gmail.com>
2024-06-04 17:22:41 -0300

    Exctract and show data about some music

--- extsite.sh
+++ extsite.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 
-# Função para exibir a tela de diálogo e capturar o link do Spotify
+# Função para mostrar a caixa de diálogo e pegar o link da música
 get_spotify_link() {
-    dialog --title "Link da Música no Spotify" --inputbox "Por favor, insira o link da música no Spotify:" 10 60 2>link.txt
-
-    response=$?
-    link=$(<link.txt)
-    rm -f link.txt
-
-    if [ $response -eq 0 ]; then
-        echo "$link"
-    else
-        dialog --msgbox "Operação cancelada." 6 40
-        exit 1
-    fi
+    dialog --inputbox "Digite o link da música do Spotify:" 8 40 2> link.txt
+    SPOTIFY_LINK=$(<link.txt)
+    rm link.txt
 }
 
-# Função para baixar o HTML da página
+# Função para baixar a página HTML usando o Lynx
 download_html() {
-    local url=$1
-    local output_file="spotify_page.html"
+    lynx -source "$SPOTIFY_LINK" > spotify_music_page.html
+}
 
-    lynx -source "$url" > "$output_file"
+# Função para extrair informações do HTML
+extract_info() {
+    TITLE=$(grep -oP '(?<=<meta property="og:title" content=").*?(?=")' spotify_music_page.html)
+    ARTIST=$(grep -oP '(?<=<meta name="music:musician_description" content=").*?(?=")' spotify_music_page.html)
+    ALBUM=$(grep -oP '(?<=<span data-encore-id="type" class="Type__TypeElement-sc-goli3j-0 dsbIME">).*?(?=</span>)' spotify_music_page.html | sed -n '1p')
+    YEAR=$(grep -oP '(?<=<span data-encore-id="type" class="Type__TypeElement-sc-goli3j-0 dsbIME">).*?(?=</span>)' spotify_music_page.html | sed -n '2p')
+    DURATION=$(grep -oP '(?<=<span data-encore-id="type" class="Type__TypeElement-sc-goli3j-0 dsbIME">).*?(?=</span>)' spotify_music_page.html | sed -n '3p')
+}
 
-    if [ $? -eq 0 ]; then
-        dialog --msgbox "HTML baixado e salvo em $output_file" 6 40
-    else
-        dialog --msgbox "Erro ao baixar o HTML." 6 40
-        exit 1
-    fi
+# Função para mostrar as informações extraídas
+show_info() {
+    INFO="Título: $TITLE\nArtista: $ARTIST\nDuração: $DURATION\nÁlbum: $ALBUM\nAno: $YEAR"
+    dialog --title "INFORMAÇÕES DA MÚSICA ESCOLHIDA" --msgbox "$INFO" 15 50
+    --ok-label "Concluir"
+    clear
 }
 
 # Função principal
 main() {
     get_spotify_link
-    download_html "$link"
-
-    if ask_extract_info; then
+    download_html
+    
+    dialog --yesno "Deseja extrair as informações da página baixada?" 7 40
+    response=$?
+    if [ $response -eq 0 ]; then
         extract_info
+        show_info
     else
-        dialog --msgbox "Operação concluída." 6 40
+        dialog --msgbox "Operação cancelada." 5 30
     fi
-
-    clear
 }
 
 # Executa a função principal
