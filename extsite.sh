b8f510fe08c4c322eb00011a15266a86b75a9d22
Arthur Sant'Anna <arthursantanna.c@gmail.com>
2024-06-04 19:33:03 -0300

    Create function to backup files

--- extsite.sh
+++ extsite.sh
@@ -19,13 +19,31 @@ extract_info() {
     ALBUM=$(grep -oP '(?<=<span data-encore-id="type" class="Type__TypeElement-sc-goli3j-0 dsbIME">).*?(?=</span>)' spotify_music_page.html | sed -n '1p')
     YEAR=$(grep -oP '(?<=<span data-encore-id="type" class="Type__TypeElement-sc-goli3j-0 dsbIME">).*?(?=</span>)' spotify_music_page.html | sed -n '2p')
     DURATION=$(grep -oP '(?<=<span data-encore-id="type" class="Type__TypeElement-sc-goli3j-0 dsbIME">).*?(?=</span>)' spotify_music_page.html | sed -n '3p')
+    CLEANED_TITLE=$(echo $TITLE | sed 's/ /_/g')
 }
 
 # Função para mostrar as informações extraídas
 show_info() {
-    INFO="Título: $TITLE\nArtista: $ARTIST\nDuração: $DURATION\nÁlbum: $ALBUM\nAno: $YEAR"
+    INFO="\n\nTítulo: $TITLE\nArtista: $ARTIST\nDuração: $DURATION\nÁlbum: $ALBUM\nAno: $YEAR"
     dialog --title "INFORMAÇÕES DA MÚSICA ESCOLHIDA" --msgbox "$INFO" 15 50
     --ok-label "Concluir"
+}
+
+# Função para criar o backup do arquivo HTML e das informações extraídas
+create_backup() {
+    dialog --msgbox "Criando backup dos arquivos..." 5 30
+    cp spotify_music_page.html "spotify_$CLEANED_TITLE.html"
+    echo -e "Título: $TITLE\nArtista: $ARTIST\nDuração: $DURATION\nÁlbum: $ALBUM\nAno: $YEAR" > "info_$CLEANED_TITLE.txt"
+    echo -e "Link do Spotify: $SPOTIFY_LINK" >> "info_$CLEANED_TITLE.txt"
+    zip -r "backup_$CLEANED_TITLE.zip" "spotify_$CLEANED_TITLE.html" "info_$CLEANED_TITLE.txt"
+    if [ -d backups ]; then
+        mv "backup_$CLEANED_TITLE.zip" backups/
+    else
+        mkdir backups
+        mv "backup_$CLEANED_TITLE.zip" backups/
+    fi
+    rm spotify_music_page.html "info_$CLEANED_TITLE.txt" "spotify_$CLEANED_TITLE.html"
+    dialog --msgbox "Backup criado com sucesso!" 5 30 --ok-label "Finalizar"
     clear
 }
 
@@ -39,6 +57,7 @@ main() {
     if [ $response -eq 0 ]; then
         extract_info
         show_info
+        create_backup
     else
         dialog --msgbox "Operação cancelada." 5 30
     fi
